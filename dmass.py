b679e65ce85e8a9bb89a3464327b18459c6986bd
NedasxdBot <65161408+NedasxdBot@users.noreply.github.com>
2020-05-11 12:40:26 +0300

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -17,7 +17,7 @@ async def on_ready():
     print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     print('--------')
     print('--------')
-    print('Sukurtas Klyčkos')
+    print('Sukurtas Klyckos')
 
 @client.command(pass_context = True)
 @commands.has_permissions(kick_members=True)     
